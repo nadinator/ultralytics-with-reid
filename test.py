0a272560cca9c7250095d026f0b2954d66a9e2fd
nba <64587372+nadinator@users.noreply.github.com>
2024-11-20 09:51:36 +0300

    Add shebang line and brazil camera ip

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
+#! /home/nboualwa/miniconda3/envs/ultrafast/bin/python
 import sys
 
-# sys.path.append("./ultralytics")
 sys.path.append("./fast_reid")
 sys.path.append("./ultralytics")
 
 from ultralytics import YOLO
 
 
-model = YOLO()
-model.track(
-    source=r"C:\Users\Nadim\Videos\lab-2.mp4",
-    persist=True,
-    show=True,
-    tracker="my_botsort.yaml",
+model = YOLO(
+    "./best_models/train/oct31_v8x--run4--optimizerAdamW_dropout0.4_lrf0.0001_lr01e-05/weights/best.pt",
 )
+
+if __name__ == "__main__":
+    model.track(
+        source="rtsp://192.168.1.3/live",
+        persist=True,
+        show=True,
+        tracker="my_botsort.yaml",
+    )
